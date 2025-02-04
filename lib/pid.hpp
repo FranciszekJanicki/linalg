127c97f57c0e1e6b2013d573d4720d6bfb126a92
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-02-04 15:46:38 +0100

    egfwf

--- lib/pid.hpp
+++ lib/pid.hpp
@@ -10,7 +10,7 @@
 namespace Linalg::Regulators {
 
     template <std::floating_point Value>
-    [[nodiscard]] auto derivative(Value const value, T const prev_value, Value const dt) -> Value
+    [[nodiscard]] auto derivative(Value const value, Value const prev_value, Value const dt) -> Value
     {
         if (dt == Value{0}) {
             throw std::runtime_error{"Division by 0!"};
@@ -28,27 +28,16 @@ namespace Linalg::Regulators {
     struct PID {
         [[nodiscard]] auto operator()(this PID& self, Value const error, Value const dt) noexcept -> Value
         {
-            return self.get_saturated_control(error, dt);
-        }
-
-        auto get_saturation_error(this PID& self) noexcept -> Value
-        {
-            return self.prev_control - self.saturate(self.prev_control);
-        }
-
-        auto get_integrated(this PID& self, Value const error) noexcept -> Value
-        {
-            if (self.ki == Value{0}) {
-                return error;
-            }
-            return error - self.kc * self.get_saturation_error() / self.ki;
+            return self.get_control(error, dt);
         }
 
         auto get_control(this PID& self, Value const error, Value const dt) noexcept -> Value
         {
             auto control{self.kp * error + self.ki * self.get_integral(error, dt) +
                          self.kd * self.get_derivative(error, dt)};
-            return self.prev_control = control;
+            auto sat_control{std::clamp(control, -self.saturation, self.saturation)};
+            self.prev_sat_error = control - sat_control;
+            return sat_control;
         }
 
         auto get_derivative(this PID& self, Value const error, Value const dt) noexcept -> Value
@@ -58,18 +47,8 @@ namespace Linalg::Regulators {
 
         auto get_integral(this PID& self, Value const error, Value const dt) noexcept -> Value
         {
-            auto integrated{self.get_integrated(error)};
-            return self.integral_value += integral(integrated, std::exchange(self.prev_integrated, integrated), dt);
-        }
-
-        auto saturate(this PID& self, Value const control) noexcept -> Value
-        {
-            return std::clamp(control, -self.saturation, self.saturation);
-        }
-
-        auto get_saturated_control(this PID& self, Value const error, Value const dt) noexcept -> Value
-        {
-            return self.saturate(self.get_control(error, dt));
+            auto integrated{self.ki == Value{0} ? error : error - self.kc * self.prev_sat_error / self.ki};
+            return self.prev_integral += integral(integrated, std::exchange(self.prev_integrated, integrated), dt);
         }
 
         Value kp{};
@@ -78,9 +57,9 @@ namespace Linalg::Regulators {
         Value kc{};
         Value saturation{};
 
-        Value integral_value{0.0F};
+        Value prev_integral{0.0F};
         Value prev_error{0.0F};
-        Value prev_control{0.0F};
+        Value prev_sat_error{0.0F};
         Value prev_integrated{0.0F};
     };
 
