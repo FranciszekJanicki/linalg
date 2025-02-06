e350e633e724dbd62ec19306e60161f8a99c5ac6
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-02-06 21:42:13 +0100

    gkjkg

--- lib/pid.hpp
+++ lib/pid.hpp
@@ -46,39 +46,51 @@ namespace Linalg::Regulators {
 
         auto get_control(this PID& self, Value const error, Value const sampling_time) noexcept -> Value
         {
-            auto control{self.proportion_gain * error + self.integral_gain * self.get_integral(error, sampling_time) +
-                         self.derivative_gain * self.get_derivative(error, sampling_time)};
-            auto sat_control{std::clamp(control, -self.saturation, self.saturation)};
-            self.prev_sat_error = control - sat_control;
-            return sat_control;
+            auto control{self.get_proportion(error) + self.get_integral(error, sampling_time) +
+                         self.get_derivative(error, sampling_time)};
+            auto saturated_control{std::clamp(control, -self.saturation, self.saturation)};
+            self.prev_sat_error = std::exchange(self.sat_error, control - saturated_control);
+            self.prev_error = error;
+            return saturated_control;
+        }
+
+        auto get_proportion(this PID& self, Value const error) noexcept -> Value {
+            return self.proportion_gain * error;
         }
 
         auto get_derivative(this PID& self, Value const error, Value const sampling_time) noexcept -> Value
         {
-            self.derivative =
-                differentiate(error, std::exchange(self.prev_error, error), sampling_time, self.derivative, self.time_constant);
-            return self.derivative;
+            self.error_derivative =
+                differentiate(error, self.prev_error, sampling_time, self.error_derivative, self.time_constant);
+            return self.derivative_gain * self.error_derivative;
         }
 
         auto get_integral(this PID& self, Value const error, Value const sampling_time) noexcept -> Value
         {
-            auto integrated{self.integral_gain == Value{0} ? error : error - self.control_gain * self.prev_sat_error / self.integral_gain};
-            self.integral += integrate(integrated, std::exchange(self.prev_integrated, integrated), sampling_time);
-            return self.integral;
+            self.error_integral += integrate(error, self.prev_error, sampling_time);
+            self.sat_error_integral += integrate(self.sat_error, self.prev_sat_error, sampling_time);
+            return self.integral_gain * self.error_integral - self.control_gain * self.sat_error_integral;
         }
 
+        // basic PID parameters
         Value proportion_gain{};
         Value integral_gain{};
         Value derivative_gain{};
-        Value control_gain{};
-        Value saturation{};
         Value time_constant{};
 
-        Value integral{0.0F};
-        Value derivative{0.0F};
+        // anti windup parameters
+        Value control_gain{};
+        Value saturation{};
+       
+        // basic PID internal state
         Value prev_error{0.0F};
+        Value error_integral{0.0F};
+        Value error_derivative{0.0F};
+
+        // anti windup internal state
+        Value sat_error{0.0F};
         Value prev_sat_error{0.0F};
-        Value prev_integrated{0.0F};
+        Value sat_error_integral{0.0F};
     };
 
 }; // namespace Linalg::Regulators
